3367a775bfa7974c98b8c393cb02e77b57928980
Bartosz Kunat <mrclapslock@gmail.com>
2017-12-23 19:02:06 +0100

    Paddle can now be controlled. Fixed paddle.position.x constraints (it cannot leave the current frame)

--- Arkanoid/GameScene.swift
+++ Arkanoid/GameScene.swift
@@ -15,6 +15,8 @@ let GameMessageName = "gameMessage"
 
 
 class GameScene: SKScene {
+   
+    var isFingerOnPaddle = false
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -28,9 +30,70 @@ class GameScene: SKScene {
         
         let ball = childNode(withName: BallCategoryName) as! SKSpriteNode
         ball.physicsBody!.applyImpulse(CGVector(dx: 1, dy: -12))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
         
-        
-        
+        if let body = physicsWorld.body(at: touchLocation) {
+            if body.node!.name == PaddleCategoryName {
+                isFingerOnPaddle = true
+                print("It works!")
+            }
+            
+        }
         
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isFingerOnPaddle {
+            let touch = touches.first
+            let touchLocation = touch!.location(in: self)
+            let previousLocation = touch!.previousLocation(in: self)
+            let paddle = childNode(withName: PaddleCategoryName) as! SKSpriteNode
+            
+            var paddleX = paddle.position.x + (touchLocation.x - previousLocation.x)
+            if paddleX < 0 {
+                paddleX = max(paddleX, -(self.frame.size.width / 2) + (paddle.size.width / 2))
+            } else if paddleX > 0 {
+                paddleX = min(paddleX, (self.frame.size.width / 2) - (paddle.size.width / 2))
+                
+            }
+            
+            paddle.position = CGPoint(x: paddleX, y: paddle.position.y)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isFingerOnPaddle = false
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
