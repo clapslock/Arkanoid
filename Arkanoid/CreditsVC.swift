21181c5ef09bf175d9720f6db43b445f9d041690
Bartosz Kunat <mrclapslock@gmail.com>
2017-12-27 02:01:31 +0100

    Completed the credits VC

---
+++ Arkanoid/CreditsVC.swift
@@ -0,0 +1,23 @@
+//
+//  CreditsVC.swift
+//  Arkanoid
+//
+//  Created by Bartosz Kunat on 27/12/2017.
+//  Copyright © 2017 Clapslock Interactive. All rights reserved.
+//
+
+import UIKit
+
+class CreditsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
